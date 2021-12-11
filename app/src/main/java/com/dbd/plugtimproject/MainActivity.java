276d3fc7272d17342d4bd162264f71595ae057a1
DumitrescuBogdanDoru <bogdandoru.dumitrescu@gmail.com>
2021-12-11 19:13:24 +0200

    M-3: added Register/Login screens + changed styles

--- app/src/main/java/com/dbd/plugtimproject/MainActivity.java
+++ app/src/main/java/com/dbd/plugtimproject/MainActivity.java
@@ -1,14 +1,43 @@
 package com.dbd.plugtimproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private EditText logUsername;
+    private EditText logPassword;
+    private Button loginBtnLog;
+    private TextView forgotBtnLog;
+    private TextView registerBtnLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logUsername = findViewById(R.id.logUsername);
+        logPassword = findViewById(R.id.logPassword);
+        loginBtnLog = findViewById(R.id.loginBtnLog);
+        forgotBtnLog = findViewById(R.id.forgotBtnLog);
+        registerBtnLog = findViewById(R.id.registerBtnLog);
+
+        registerBtnLog.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.registerBtnLog:
+                startActivity(new Intent(getApplicationContext(), Register.class));
+                break;
+        }
     }
 }
