ea60d6e4af88b00ee0635ba45f0de5dc093a5638
DumitrescuBogdanDoru <bogdandoru.dumitrescu@gmail.com>
2022-05-19 18:05:39 +0300

    W-3: added bottom nav bar with fab

---
+++ app/src/main/java/com/dbd/plugtimproject/activities/MainActivity.java
@@ -0,0 +1,24 @@
+package com.dbd.plugtimproject.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dbd.plugtimproject.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        // disable holder item
+        bottomNavigationView.getMenu().getItem(2).isEnabled();
+    }
+}
