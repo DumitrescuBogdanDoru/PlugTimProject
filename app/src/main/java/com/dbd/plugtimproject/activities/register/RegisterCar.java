49c90de8d1abfbe081d89b15d25065f5f8641a78
DumitrescuBogdanDoru <bogdandoru.dumitrescu@gmail.com>
2022-05-23 18:09:41 +0300

    W-4: added uploading car image functionality

--- app/src/main/java/com/dbd/plugtimproject/activities/register/RegisterCar.java
+++ app/src/main/java/com/dbd/plugtimproject/activities/register/RegisterCar.java
@@ -1,20 +1,32 @@
 package com.dbd.plugtimproject.activities.register;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dbd.plugtimproject.R;
 import com.dbd.plugtimproject.models.Car;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.Calendar;
 
@@ -26,6 +38,13 @@ public class RegisterCar extends AppCompatActivity implements View.OnClickListen
     private EditText regCarCompany, regCarModel, regCarColor, regCarYear;
 
     private DatabaseReference mDatabase;
+
+    private Button addCarImageBtn;
+    private ImageView carImage;
+
+    private Uri imageUri;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
 
 
     @Override
@@ -42,6 +61,14 @@ public class RegisterCar extends AppCompatActivity implements View.OnClickListen
         regCarModel = findViewById(R.id.regCarModel);
         regCarColor = findViewById(R.id.regCarColor);
         regCarYear = findViewById(R.id.regCarYear);
+
+        // add car image
+        addCarImageBtn = findViewById(R.id.addCarImageBtn);
+        addCarImageBtn.setOnClickListener(this);
+        carImage = findViewById(R.id.carImage);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
     }
 
     @Override
@@ -55,7 +82,11 @@ public class RegisterCar extends AppCompatActivity implements View.OnClickListen
                 String email = intent.getStringExtra("email");
                 if (registerCar(email)) {
                     startActivity(new Intent(getApplicationContext(), Login.class));
+                    uploadPicture(FirebaseAuth.getInstance().getCurrentUser().getUid());
                 }
+                break;
+            case R.id.addCarImageBtn:
+                addCarImage();
                 break;
         }
     }
@@ -101,5 +132,62 @@ public class RegisterCar extends AppCompatActivity implements View.OnClickListen
         });
 
         return true;
+    }
+
+    private void addCarImage() {
+        // intent to open gallery from phone
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            carImage.setImageURI(imageUri);
+        }
+    }
+
+    private void uploadPicture(String uuid) {
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setTitle("Uploading image");
+        pd.show();
+
+        StorageReference stationReference = storageReference.child("images/car/" + uuid);
+        stationReference.listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+
+
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+
+
+                        stationReference.putFile(imageUri)
+                                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                                        pd.dismiss();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        pd.dismiss();
+                                        Toast.makeText(RegisterCar.this, "Couldn't load image", Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                                        double progress = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                                        pd.setMessage("Progress: " + (int) progress + "%");
+                                    }
+                                });
+                    }
+                });
     }
 }
